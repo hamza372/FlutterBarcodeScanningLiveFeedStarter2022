100a9d71c735dba198bf01342dfc1a4bcf9875de
hamza372 <31725222+hamza372@users.noreply.github.com>
2022-10-15 08:52:19 -0700

    2022

---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+
+late List<CameraDescription> _cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
+  runApp(const CameraScreen());
+}
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+  CameraImage? img;
+  bool isBusy = false;
+  String result = "results will be shown";
+
+  //TODO declare scanner
+  dynamic barcodeScanner;
+  @override
+  void initState() {
+    super.initState();
+    //TODO initialize scanner
+    final List<BarcodeFormat> formats = [BarcodeFormat.all];
+    barcodeScanner = BarcodeScanner(formats: formats);
+
+    //TODO initialize the controller
+    controller = CameraController(_cameras[0], ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      controller.startImageStream((image) => {
+            if (!isBusy) {isBusy = true, img = image, doBarcodeScanning()}
+          });
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print('User denied camera access.');
+            break;
+          default:
+            print('Handle other errors.');
+            break;
+        }
+      }
+    });
+  }
+
+  //TODO barcode scanning code here
+  doBarcodeScanning() async {}
+
+  InputImage getInputImage() {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (final Plane plane in img!.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+    final Size imageSize = Size(img!.width.toDouble(), img!.height.toDouble());
+    final camera = _cameras[0];
+    final imageRotation =
+        InputImageRotationValue.fromRawValue(camera.sensorOrientation);
+    // if (imageRotation == null) return;
+
+    final inputImageFormat =
+        InputImageFormatValue.fromRawValue(img!.format.raw);
+    // if (inputImageFormat == null) return null;
+
+    final planeData = img!.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation!,
+      inputImageFormat: inputImageFormat!,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    return inputImage;
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Stack(
+        fit: StackFit.expand,
+        children: [
+          CameraPreview(controller),
+          Container(
+            margin: const EdgeInsets.only(left: 10, bottom: 10),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                result,
+                style: const TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
